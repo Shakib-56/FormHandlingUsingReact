631ebcfba680f5dbf522668607f15f34111a68f6
shakib <hossenmdshakib47@gmail.com>
2025-04-24 15:27:11 +0600

    add a form and get data using event.target

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import SimpleForm from './components/simpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+   <h1>Vite + React</h1>
+   <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
